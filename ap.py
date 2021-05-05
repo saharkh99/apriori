0357df31f7a57a095d34f3f804290fff35a0f8eb
sahar khazali <saharkh9977@gmail.com>
2021-05-05 15:25:17 +0430

    add functions

--- ap.py
+++ ap.py
@@ -1,7 +1,11 @@
 import csv
+import itertools
 from collections import defaultdict
 
-def getTransListSet( filePath='x.csv'):
+import options as options
+
+
+def getTransListSet(filePath='x.csv'):
     transListSet = []
     with open(filePath, 'r') as file:
         reader = csv.reader(file, delimiter=',')
@@ -9,28 +13,32 @@ def getTransListSet( filePath='x.csv'):
             transListSet.append(set(line))
     return transListSet
 
+
 def getOneItemSet(transListSet):
     itemSet = set()
     for line in transListSet:
         for item in line:
-            itemSet.add(frozenset([item]))
+            if str(item):
+                itemSet.add(frozenset([item]))
     return itemSet
 
-def fillItemCountDict(itemSet,transListSet):
+
+def fillItemCountDict(itemSet, transListSet):
     localSet_ = defaultdict(int)
     for item in itemSet:
         localSet_[item] += sum([1 for trans in transListSet if item.issubset(trans)])
-        print(item)
+        # print(item)
     return localSet_
 
 
 def getJoinedItemSet(termSet, k):
     return set([term1.union(term2) for term1 in termSet for term2 in termSet
-                if len(term1.union(term2))==k])
+                if len(term1.union(term2)) == k])
 
-def getSupport(itemCount,transLength):
-    print(itemCount)
+
+def getSupport(itemCount, transLength):
     return itemCount / transLength
+
 
 def getItemsWithMinSupp(transListSet, itemSet, minSupp):
     itemSet_ = set()
@@ -40,25 +48,59 @@ def getItemsWithMinSupp(transListSet, itemSet, minSupp):
     n = len(transListSet)
     for item, cnt in localSet_.items():
         itemSet_.add(item) if float(cnt) / n >= minSupp else None
+
     return itemSet_
 
 
-def checkConfidence(itemCountDict,minConf):
+def checkConfidence(itemCountDict, minConf, tranlen):
     rules = dict()
     for key, value in itemCountDict.items():
-        for item in value:
-            item_supp = getSupport(item)
-            conf = item_supp / getSupport(item)
-            if conf >= minConf:
-                rules[item] = conf
+        item_supp = getSupport(value, tranlen)
+        for item in key:
+           conf = item_supp / getSupport(itemCountDict[item], tranlen)
+           if conf >= minConf:
+               rules[key] = conf
     return rules
 
-
+def getCombinations(freqSe):
+    perm=[]
+    dict_rules=dict()
+    li=[]
+    x=0
+    for r in freqSe:
+        perm = itertools.permutations(r)
+        for i in perm:
+           li=list(i)
+           temp_i = li[:]
+           for j in range(len(li)-1):
+               k = [x for index, x in enumerate(temp_i) if index <= j]
+               m = [x for x in li if x not in k]
+               print(str(k)+"->"+str(m))
 if __name__ == '__main__':
+    freqSet = dict()
     itemCountDict = defaultdict(int)
     transLength = len(getTransListSet())
     itemSet = getOneItemSet(getTransListSet())
-    itemCountDict=fillItemCountDict(itemSet,getTransListSet())
-    print(getJoinedItemSet(itemSet,1))
-    print(getSupport(itemCountDict[frozenset({'whole milk'})],transLength))
-    print(getTransListSet(),itemSet,2)
+    itemCountDict = fillItemCountDict(itemSet, getTransListSet())
+    freqOneTermSet = getItemsWithMinSupp(getTransListSet(), itemSet, 0.005)
+    k = 1
+    currFreqTermSet = dict()
+    l=[]
+    currFreqTermSet = freqOneTermSet
+    # l.append([list(x) for x in currFreqTermSet])
+    while currFreqTermSet != set():
+        freqSet[k] = currFreqTermSet
+        k += 1
+        currCandiItemSet = getJoinedItemSet(currFreqTermSet, k)
+        currFreqTermSet = getItemsWithMinSupp(getTransListSet(), currCandiItemSet, 0.005)
+        itemCountDict = fillItemCountDict(currFreqTermSet, getTransListSet())
+        # print(itemCountDict.items())
+        # print("frequent item sets")
+        # print()
+        l.append([list(x) for x in currFreqTermSet])
+
+        # print("confidence with rules")
+        # print(checkConfidence(itemCountDict, 0.2, transLength))
+# confidence- lift
+    for i in l:
+       print(getCombinations(i))
