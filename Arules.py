77f4ae7f7244f1f0c5ac17bd1d936454356b9368
sahar khazali <saharkh9977@gmail.com>
2021-05-09 14:25:17 +0430

    add functions

--- Arules.py
+++ Arules.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 
 
 def getOneItemSet(transListSet):
+    # get all the transactions and save it in itemSet
     itemSet = set()
     for line in transListSet:
         for item in line:
@@ -13,38 +14,42 @@ def getOneItemSet(transListSet):
 
 
 def fillItemCountDict(itemSet, transListSet):
-    localSet_ = defaultdict(int)
+    # set dictionary with items and their counts
+    localSetForCounts = defaultdict(int)
     for item in itemSet:
         l = [x for x in item]
-        localSet_[str(l)] += sum([1 for trans in transListSet if item.issubset(trans)])
-    return localSet_
+        localSetForCounts[str(l)] += sum([1 for trans in transListSet if item.issubset(trans)])
+    return localSetForCounts
 
 
 def getJoinedItemSet(termSet, k):
+    # join terms to make k-term
     return set([term1.union(term2) for term1 in termSet for term2 in termSet
                 if len(term1.union(term2)) == k])
 
 
 def getSupport(itemCount, transLength):
+    # support for term
     return itemCount / transLength
 
 
 def getItemsWithMinSupp(transListSet, itemSet, minSupp):
+    # compare with min support in each itemset
     itemSet_ = set()
     localSet_ = defaultdict(int)
     for item in itemSet:
         localSet_[item] += sum([1 for trans in transListSet if item.issubset(trans)])
     n = len(transListSet)
-    for item, cnt in localSet_.items():
-        itemSet_.add(item) if float(cnt) / n >= minSupp else None
-
+    for item, count in localSet_.items():
+        itemSet_.add(item) if float(count) / n >= minSupp else None
     return itemSet_
 
 
-def getCombinations(freqSe, itemCountDict,min_confidence,lentrans):
-    rules=[]
-    lifts=[]
-    lift=1
+def getRules(freqSe, itemCountDict, min_confidence, lentrans):
+    # make rules(k->m) from frequent itemSets with this pattern:temp_i{k,m}
+    rules = []
+    lifts = []
+    lift = 1
     for r in freqSe:
         perm = itertools.permutations(r)
         for i in perm:
@@ -56,28 +61,35 @@ def getCombinations(freqSe, itemCountDict,min_confidence,lentrans):
                 b1 = getSupport(itemCountDict[str(k)], lentrans)
                 b2 = getSupport(itemCountDict[str(temp_i)], lentrans)
                 b3 = getSupport(itemCountDict[str(m)], lentrans)
-                if(b3*b1!=0):
-                   lift= b2/b1*b3
-                if (b2 != 0):
-                    confidence = b1 / b2
+                if (b3 * b1 != 0):
+                    lift = b2 / b1 * b3
+                if (b1 != 0):
+                    confidence = b2 / b1
                     if (confidence >= min_confidence):
-                        elm=str(k) + "->" + str(m)
-                        if(elm!=None):
-                           lifts.append(lift)
-                           rules.append(elm)
-    dicta=dict(zip(rules,lifts))
-    [print(key, value) for (key, value) in sorted(dicta.items(), key=lambda x: x[1])]
+                        elm = str(k) + "->" + str(m) + " , " + str(confidence)
+                        if (elm != None):
+                            lifts.append(lift)
+                            rules.append(elm)
+    dicta = dict(zip(rules, lifts))
+    [print(key) for (key, value) in sorted(dicta.items(), key=lambda x: x[1], reverse=True)]
+
+
 def getTransListSet(filePath='groceries.csv'):
-        transListSet = []
-        with open(filePath, 'r') as file:
-            reader = csv.reader(file, delimiter=',')
-            for line in reader:
-                transListSet.append(set(line))
-        return transListSet
+    # get file.csv
+    transListSet = []
+    with open(filePath, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        for line in reader:
+            transListSet.append(set(line))
+    return transListSet
+
+
 class Arules:
     dicta = defaultdict(int)
     l = []
-    def get_frequent_item_sets(self,transactions=getTransListSet(),min_support=0.005, min_confidence=0.2):
+
+    def get_frequent_item_sets(self, transactions=getTransListSet(), min_support=0.005):
+        # initial itemCountdict and freqOneSet for k=1
         freqSet = dict()
         itemCountDict = defaultdict(int)
         transLength = len(transactions)
@@ -93,15 +105,15 @@ class Arules:
             freqSet[k] = currFreqTermSet
             k += 1
             currCandiItemSet = getJoinedItemSet(currFreqTermSet, k)
-            currFreqTermSet = getItemsWithMinSupp(transactions, currCandiItemSet,min_support)
+            currFreqTermSet = getItemsWithMinSupp(transactions, currCandiItemSet, min_support)
             itemCountDict = fillItemCountDict(currFreqTermSet, transactions)
             self.dicta.update(itemCountDict)
-            if(len(currFreqTermSet)>0):
-              self.l.append([list(x) for x in currFreqTermSet])
+            if (len(currFreqTermSet) > 0):
+                self.l.append([list(x) for x in currFreqTermSet])
         return self.l
 
-    def get_arules(self, min_support=0.005, min_confidence=0.2,min_lift=None,sort_by='lift'):
-         self.get_frequent_item_sets()
-         for i in self.l:
-             if(i!=None):
-                getCombinations(i, self.dicta, min_confidence, len(getTransListSet()))
+    def get_arules(self, min_support=0.005, min_confidence=0.2, min_lift=None, sort_by='lift'):
+        self.get_frequent_item_sets()
+        for i in self.l:
+            if (i != None):
+                getRules(i, self.dicta, min_confidence, len(getTransListSet()))
